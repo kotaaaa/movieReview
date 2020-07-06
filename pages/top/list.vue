32605e61a161d85a3aa9e9a307a827cacfb68adf
kotaaaa <kota.k.1132.pda@gmail.com>
2020-07-06 17:58:13 +0900

    get movie list with json

---
+++ pages/top/list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div style="text-align:center">
+    <p> 放映中の映画一覧 </p>
+    <p> {{ ip }} </p>
+    <button @click="getIp"> 映画一覧を取得 </button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data: function(){
+    return {
+      ip: 'リスト(json)'
+    }
+  } ,
+  methods: {
+    async getIp(){
+      this.ip = await this.$axios.$get('https://api.themoviedb.org/3/movie/now_playing?api_key=0534930d6692ebb101db3214a44bcfe7')  
+    }
+  }
+}
+</script>
